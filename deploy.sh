6deba284c5673c830242ee93936cd0d07bfd6578
cconecode <18899851894@163.com>
2019-12-05 15:33:18 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,20 @@
 hugo
 
+echo 'Hugo done!'
+
 cd public
+
+git add .
+
+git commit -m "update"
+
+git push origin master
+
+git push --force git@git.dev.tencent.com:superchun/superchun.coding.me.git master:master
+
+echo 'deploy done!'
+
+cd ..
 
 git add .
 

9197e785060934c9c5c511c048a051edb3034610
cc <cc@xidaduo-2.local>
2021-08-11 09:29:53 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,6 @@ hugo
 
 echo 'Hugo done!'
 
-cd public
-
-git add .
-
-git commit -m "update"
-
-git push origin master
-
-git push --force git@git.dev.tencent.com:superchun/superchun.coding.me.git master:master
-
-echo 'deploy done!'
-
-cd ..
-
 git add .
 
 git commit -m "update"
